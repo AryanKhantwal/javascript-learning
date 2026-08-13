2bd5d3e850ff3a1d64a832dead9c206f4e7619d4
user.email "aryankahntwal396@gmail.com <aryankhantwal396@gmail.com>
2026-08-14 03:49:50 +0530

    feat: organize variables and data types

---
+++ 01-basics/02-data-types.js
@@ -0,0 +1,35 @@
+// String
+let name = "Aryan";
+
+// Number
+let age = 22;
+
+// Boolean
+let isStudent = true;
+
+// Undefined
+let city;
+
+// Null
+let selectedCourse = null;
+
+// BigInt
+let bigNumber = 12345678901234567890n;
+
+// Symbol
+let id = Symbol("id");
+
+// Object
+let student = {
+    name: "Aryan",
+    age: 22
+};
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(city);
+console.log(selectedCourse);
+console.log(bigNumber);
+console.log(id);
+console.log(student);
