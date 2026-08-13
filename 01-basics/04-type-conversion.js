d98c0d2112d5b9bbc1012d9957e4a3ac3a3d8e9a
user.email "aryankahntwal396@gmail.com <aryankhantwal396@gmail.com>
2026-08-14 04:10:19 +0530

    feat: add JavaScript type conversion

---
+++ 01-basics/04-type-conversion.js
@@ -0,0 +1,23 @@
+// String to Number
+let age = "22";
+let convertedAge = Number(age);
+
+console.log(convertedAge);
+console.log(typeof convertedAge);
+
+// Number to String
+let score = 100;
+let convertedScore = String(score);
+
+console.log(convertedScore);
+console.log(typeof convertedScore);
+
+// Boolean conversion
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean("Hello"));
+console.log(Boolean(""));
+
+// Implicit conversion
+console.log("10" + 5);
+console.log("10" - 5);
